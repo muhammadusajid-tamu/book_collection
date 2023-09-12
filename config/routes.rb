f22067dad082e3f33ec46a642355e8884fddfae1
muhammadusajid <muhammadusajid@tamu.edu>
2023-09-12 18:51:45 +0000

    Updated routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,10 @@
 Rails.application.routes.draw do
-  resources :books
-  root 'books#index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-  # config/routes.rb
-
-resources :books do
-  member do
-    get 'confirm_destroy'
+  resources :books do
+    member do
+      get 'confirm_destroy'
+    end
   end
-end
 
+  root 'books#index'
+  # Other routes go here if needed
 end
